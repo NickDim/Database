d3323226c4f237158a903f4616b6e93431f63886
NickDim <nikolad21889@isd273.org>
2017-10-14 10:30:33 -0500

    Made code cleaner

--- src/Database.java
+++ src/Database.java
@@ -3,29 +3,54 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.*;
 
 public class Database {
+
     public static void main(String args[]){
-        Database database = new Database();
-        String uifName = "''";
-        String uilName = "''";
-        database.insertStatement(uifName, uilName);
-        database.selectStatement();
+        try {
+
+            Database database = new Database();
+
+            String uifName = "'Velin'";
+            String uilName = "'Dimitrov'";
+
+            Connection con = database.getConnection();
+            database.insertStatement(uifName, uilName);
+            database.selectStatement();
+
+            con.commit();
+            con.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private MysqlDataSource dataSource;
+    private Connection con;
+
+    private Connection getConnection() {
+        return con;
+    }
+
+    public Database() throws SQLException {
+
+        this.dataSource = new MysqlDataSource();
+
+        dataSource.setUser("root");
+        dataSource.setPassword("password");
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("nickdim");
+
+        this.con = dataSource.getConnection();
+        con.setAutoCommit(false);
     }
 
     public void insertStatement(String uifName, String uilName) {
         try {
-            MysqlDataSource dataSource = new MysqlDataSource();
-            dataSource.setUser("root");
-            dataSource.setPassword("password");
-            dataSource.setServerName("localhost");
-            dataSource.setPort(3306);
-            dataSource.setDatabaseName("nickdim");
 
-            Connection con = dataSource.getConnection();
-            con.setAutoCommit(false);
             PreparedStatement stmt = con.prepareStatement("INSERT INTO nickdimfans (FirstName, LastName)\nVALUES (+ " + uifName + "," + uilName + ")");
             stmt.executeUpdate();
-            con.commit();
-            con.close();
+
         }
         catch(SQLException e){
             e.printStackTrace();
@@ -34,33 +59,23 @@ public class Database {
 
     public void selectStatement() {
         try {
-            MysqlDataSource dataSource = new MysqlDataSource();
-            dataSource.setUser("root");
-            dataSource.setPassword("password");
-            dataSource.setServerName("localhost");
-            dataSource.setPort(3306);
-            dataSource.setDatabaseName("nickdim");
 
-            Connection con = dataSource.getConnection();
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM nickdimfans");
 
-            con.setAutoCommit(false);
-
             if (con != null) {
                 StringBuilder selectStatement = new StringBuilder();
+                selectStatement.append("ID\tFirst Name\tLast Name\n");
                 while(rs.next()) {
-                    selectStatement.append("ID: ");
                     selectStatement.append(rs.getInt("ID"));
-                    selectStatement.append("\t First Name: ");
+                    selectStatement.append("\t");
                     selectStatement.append(rs.getString("FirstName"));
-                    selectStatement.append("\t Last Name: ");
-                    selectStatement.append(rs.getString("LastName") + "\n");
+                    selectStatement.append("\t");
+                    selectStatement.append(rs.getString("LastName"));
+                    selectStatement.append("\n");
                 }
                 System.out.println(selectStatement);
-                con.commit();
             }
-            con.close();
         }
         catch(SQLException e){
             e.printStackTrace();
